7f5b2ca2d12775c3dcde7e366aee18419ec963fb
_Algorithm_Kid98 <30970161+immanuelsavio@users.noreply.github.com>
2019-09-10 21:24:56 +0530

    added linked list

---
+++ Data_Structures/Linked List/nth_from_back.cpp
@@ -0,0 +1,30 @@
+ // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *node = head;
+        ListNode *prev = head;
+        int count = 0;
+        while(node->next != NULL)
+        {
+            count++;
+            node = node->next;
+        }
+        n = count - n;
+        node = head;
+        while(n>0)
+        {
+            n--;
+            node = node->next;
+            
+        }
+        node->next = (node->next)->next;
+        return head;
+    }
+};
